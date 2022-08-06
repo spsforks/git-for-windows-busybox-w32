bfcd8189fa342989fe14ffcba56ed7621a98b79f
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-08-06 17:18:37 +0200

    Add a `cygpath` applet
    This is just a minimal stand-in for Cygwin's/MSYS2's tool.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ win32/path-convert.h
@@ -0,0 +1,15 @@
+/* vi: set sw=4 ts=4 expandtab: */
+#ifndef PATH_CONVERT_H
+#define PATH_CONVERT_H
+
+enum path_convert_flag {
+    PATH_CONVERT_ABSOLUTE  = (1 << 0),
+    PATH_CONVERT_MIXED     = (1 << 1),
+    PATH_CONVERT_UNIX      = (1 << 2),
+    PATH_CONVERT_WINDOWS   = (1 << 3),
+};
+
+const char *path_convert(const char *path, char *output, size_t output_size,
+                         enum path_convert_flag flags);
+
+#endif
