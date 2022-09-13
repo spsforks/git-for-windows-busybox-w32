ed10b8ddf7d16e52d54590331e584a475e0e9904
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-09-13 13:32:26 +0200

    cygpath: support the `-p` flag to convert path lists
    Path lists (think: `PATH`) are colon or semicolon-separated lists of
    paths. These can be Windows-style, Unix-style and mixed.
    
    Let's teach the `cygpath` utility to convert also path _lists_ between
    them styles.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- win32/path-convert.c
+++ win32/path-convert.c
@@ -2,8 +2,10 @@
 #include "libbb.h"
 #include "path-convert.h"
 
-const char *path_convert(const char *path, char *output, size_t output_size,
-                         enum path_convert_flag flags)
+static const char *path_convert_internal(const char *path,
+                                         char *output, size_t output_size,
+                                         size_t *output_size_hint,
+                                         enum path_convert_flag flags)
 {
     char *p;
     DWORD len;
@@ -15,13 +17,18 @@ const char *path_convert(const char *path, char *output, size_t output_size,
             wpath += 4;
 
         len = WideCharToMultiByte(CP_UTF8, 0, wpath, -1, NULL, 0, NULL, NULL);
-        if (len > output_size)
+        if (len > output_size) {
+            *output_size_hint = len;
             return NULL;
-        if (WideCharToMultiByte(CP_UTF8, 0, wpath, -1, output, output_size, NULL, NULL) == 0)
+        }
+        if (WideCharToMultiByte(CP_UTF8, 0, wpath, -1, output, output_size, NULL, NULL) == 0) {
+            *output_size_hint = len;
             return NULL;
-    } else if ((len = strlen(path)) + 1 > output_size)
+        }
+    } else if ((len = strlen(path)) + 1 > output_size) {
+        *output_size_hint = len + 1;
         return NULL;
-    else
+    } else
         safe_strncpy(output, path, output_size);
 
     if ((flags & (PATH_CONVERT_WINDOWS & PATH_CONVERT_MIXED)) &&
@@ -75,4 +82,78 @@ const char *path_convert(const char *path, char *output, size_t output_size,
     }
 
     return output;
+}
+
+const char *path_convert(const char *path, char *output, size_t output_size,
+                         enum path_convert_flag flags)
+{
+    size_t unused;
+
+    return path_convert_internal(path, output, output_size, &unused, flags);
+}
+
+static void grow_result(char **result, size_t count, size_t actual, size_t *alloc)
+{
+    if (actual + count <= *alloc)
+        return;
+    *alloc = MAX(actual + count + 64, *alloc + (*alloc >> 1));
+    *result = xrealloc(*result, *alloc);
+}
+
+char *path_convert_path_list(const char *path_list,
+                             enum path_convert_flag flags)
+{
+    size_t alloc = strlen(path_list), count = 0, len, hint;
+    char *result = xmalloc(alloc);
+    const char *path_start = path_list, *p = path_list, *p2, *p3;
+
+    if (!path_list || !*path_list) {
+        bb_error_msg("cannot convert empty path");
+        return NULL;
+    }
+
+    /* find path separator, if any */
+    for (;;) {
+        if (*p && *p != ';' &&
+            /*
+             * Be nice and handle colon-separated path lists, too, but
+             * take care of _not_ mishandling a colon after a drive letter.
+             */
+            (*p != ':' || (p == path_start + 1 && isalpha(*path_start)))) {
+            p++;
+            continue;
+        }
+
+        if (p == path_start)
+            p2 = ".";
+        else if (!*p)
+            p2 = path_start;
+        else {
+            grow_result(&result, p + 1 - path_start, count, &alloc);
+            safe_strncpy(result + count, path_start, p + 1 - path_start);
+            p2 = NULL; /* do not point into `result` as it might be realloced, use `result + count` instead */
+        }
+
+        p3 = path_convert_internal(p2 ? p2 : result + count, result + count, alloc - count, &hint, flags);
+        if (!p3) {
+            grow_result(&result, hint, count, &alloc);
+            p3 = path_convert_internal(p2 ? p2 : result + count, result + count, alloc - count, &hint, flags);
+            if (!p3)
+                bb_error_msg_and_die("tried to realloc, but still %lu bytes missing", (unsigned long)hint);
+        }
+
+        len = strlen(p3);
+        if (p3 != result + count)
+            memmove(result + count, p3, len + 1);
+
+        count += len;
+        if (!*p) {
+            result[count] = '\0';
+            return result;
+        }
+
+        grow_result(&result, 2, count, &alloc);
+        result[count++] = flags & PATH_CONVERT_UNIX ? ':' : ';';
+        path_start = ++p;
+    }
 }
