shunf4/busybox-w32

mingw: special-case spawnve()ing MSYS2 programs
MSYS2 has funny quoting rules... So we have to accommodate for it by
quoting specifically when we detect that the executable is MSYS2 (for
which we use the tell-tale that the .exe lives in usr/bin/).

As the MSYS2 quoting rules are different from the regular Win32 ones,
and as we already handle the regular Win32 case, we now need to be
careful to choose either one or the other.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

win32/process.c
@@ -112,12 +112,42 @@ parse_interpreter(const char *cmd, char ***opts, int *nopts)
 	return p+1;
 }
 
+static char *quote_arg_msys2(const char *arg)
+{
+	int escapes = 0, has_white_space = 0;
+	const char *p;
+	char *q, *result;
+
+	for (p = arg; *p; p++)
+		if (isspace(*p))
+			has_white_space = 1;
+		else if (*p == '"' || *p == '\\')
+			escapes++;
+
+	if (!escapes && !has_white_space && p != arg)
+		return (char *)arg;
+
+	q = result = malloc(p - arg + escapes + 3);
+	if (!q)
+		return (char *)arg; /* out of memory: punt */
+	*(q++) = '"';
+	for (p = arg; *p; p++) {
+		if (*p == '"' || *p == '\\')
+			*(q++) = '\\';
+		*(q++) = *p;
+	}
+	*(q++) = '"';
+	*q = '\0';
+
+	return result;
+}
+
 /*
  * See http://msdn2.microsoft.com/en-us/library/17w5ykft(vs.71).aspx
  * (Parsing C++ Command-Line Arguments)
  */
 static char *
-quote_arg(const char *arg)
+quote_arg_mingw(const char *arg)
 {
 	int len = 0, n = 0;
 	int force_quotes = 0;
@@ -207,19 +237,39 @@ quote_arg(const char *arg)
 	return q;
 }
 
+static inline int is_slash(char c)
+{
+	return c == '/' || c == '\\';
+}
+
+static int is_msys2_cmd(const char *cmd)
+{
+	int len = strlen(cmd);
+
+	if (len < 9)
+		return 0;
+
+	while (len-- && !is_slash(cmd[len]))
+		; /* do nothing */
+	return len > 7 && is_slash(cmd[len - 8]) && is_slash(cmd[len - 4]) &&
+		!_strnicmp(cmd + len - 7, "usr", 3) &&
+		!_strnicmp(cmd + len - 3, "bin", 3);
+}
+
 static intptr_t
 spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 {
 	char **new_argv;
 	int i, argc = 0;
 	intptr_t ret;
+	char *(*quote_arg)(const char *);
 
 	if (!argv) {
 		char *const empty_argv[] = { (char *)path, NULL };
 		return spawnve(mode, path, empty_argv, env);
 	}
 
-
+	quote_arg = is_msys2_cmd(path) ? quote_arg_msys2 : quote_arg_mingw;
 	while (argv[argc])
 		argc++;
 

spsforks/git-for-windows-busybox-w32

win32/process.c
@@ -106,6 +106,37 @@ parse_interpreter(const char *cmd, interp_t *interp)
 	return 0;
 }
 
+static char * FAST_FUNC
+quote_arg_msys2(const char *arg)
+{
+	int escapes = 0, has_white_space = 0;
+	const char *p;
+	char *q, *result;
+
+	for (p = arg; *p; p++)
+		if (isspace(*p))
+			has_white_space = 1;
+		else if (*p == '"' || *p == '\\')
+			escapes++;
+
+	if (!escapes && !has_white_space && p != arg)
+		return (char *)arg;
+
+	q = result = malloc(p - arg + escapes + 3);
+	if (!q)
+		return (char *)arg; /* out of memory: punt */
+	*(q++) = '"';
+	for (p = arg; *p; p++) {
+		if (*p == '"' || *p == '\\')
+			*(q++) = '\\';
+		*(q++) = *p;
+	}
+	*(q++) = '"';
+	*q = '\0';
+
+	return result;
+}
+
 /*
  * See https://docs.microsoft.com/en-us/cpp/cpp/main-function-command-line-args?view=vs-2019#parsing-c-command-line-arguments
  * (Parsing C++ Command-Line Arguments)
@@ -150,6 +181,25 @@ find_first_executable(const char *name)
 	return find_executable(name, &path);
 }
 
+static inline int is_slash(char c)
+{
+	return c == '/' || c == '\\';
+}
+
+static int is_msys2_cmd(const char *cmd)
+{
+	int len = strlen(cmd);
+
+	if (len < 9)
+		return 0;
+
+	while (len-- && !is_slash(cmd[len]))
+		; /* do nothing */
+	return len > 7 && is_slash(cmd[len - 8]) && is_slash(cmd[len - 4]) &&
+		!_strnicmp(cmd + len - 7, "usr", 3) &&
+		!_strnicmp(cmd + len - 3, "bin", 3);
+}
+
 static intptr_t
 spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 {
@@ -159,6 +209,9 @@ spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 	intptr_t ret;
 	struct stat st;
 	size_t len = 0;
+
+	char *(FAST_FUNC *qa)(const char *);
+	qa = is_msys2_cmd(path) ? quote_arg_msys2 : quote_arg;
 
 	path = mingw_pathconv(path);
 
@@ -179,7 +232,7 @@ spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 	argc = string_array_len((char **)argv);
 	new_argv = xzalloc(sizeof(*argv)*(argc+1));
 	for (i = 0; i < argc; i++) {
-		new_argv[i] = quote_arg(argv[i]);
+		new_argv[i] = qa(argv[i]);
 		len += strlen(new_argv[i]) + 1;
 	}
 