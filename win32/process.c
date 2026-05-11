gitforwindowsgit/busybox-w32

process: special-case `cmd //c <...>`
In MSYS2's Bash, this used to be the way to ensure that `/c` is not
converted to `C:\` (in the meantime, `cmd /c <...>` is special-cased
_not_ to do that).

Let's be nice and support that special-case in BusyBox, too. This is
needed to let Git's t3700.56 "can add files via NTFS junctions" pass.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

win32/process.c
@@ -453,6 +453,17 @@ static int is_msys2_cmd(const char *cmd)
 		!_strnicmp(cmd + len - 3, "bin", 3);
 }
 
+static int is_cmd_exe(const char *cmd)
+{
+	int len = strlen(cmd);
+
+	if (len > 4 && !_strnicmp(cmd + len - 4, ".exe", 4))
+		len -= 4;
+
+	return len > 4 && is_slash(cmd[len - 4]) &&
+		!_strnicmp(cmd + len - 3, "cmd", 3);
+}
+
 static intptr_t
 spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 {
@@ -484,7 +495,10 @@ spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 	argc = string_array_len((char **)argv);
 	new_argv = xzalloc(sizeof(*argv)*(argc+1));
 	for (i = 0;i < argc;i++)
-		new_argv[i] = quote_arg(argv[i]);
+		new_argv[i] =
+			i == 1 && !strcmp(argv[1], "//c") && is_cmd_exe(path) ?
+			xstrdup("/c") :
+			quote_arg(argv[i]);
 
 	/* Special case:  spawnve won't execute a batch file if the first
 	 * argument is a relative path containing forward slashes.  Absolute

spsforks/git-for-windows-busybox-w32

win32/process.c
@@ -386,6 +386,17 @@ static int is_msys2_cmd(const char *cmd)
 		!_strnicmp(cmd + len - 3, "bin", 3);
 }
 
+static int is_cmd_exe(const char *cmd)
+{
+	int len = strlen(cmd);
+
+	if (len > 4 && !_strnicmp(cmd + len - 4, ".exe", 4))
+		len -= 4;
+
+	return len > 4 && is_slash(cmd[len - 4]) &&
+		!_strnicmp(cmd + len - 3, "cmd", 3);
+}
+
 static intptr_t
 spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 {
@@ -418,7 +429,10 @@ spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 	argc = string_array_len((char **)argv);
 	new_argv = xzalloc(sizeof(*argv)*(argc+1));
 	for (i = 0; i < argc; i++) {
-		new_argv[i] = qa(argv[i]);
+		new_argv[i] =
+			i == 1 && !strcmp(argv[1], "//c") && is_cmd_exe(path) ?
+			xstrdup("/c") :
+			qa(argv[i]);
 		len += strlen(new_argv[i]) + 1;
 	}
 