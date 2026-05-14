shunf4/busybox-w32

mingw: try to kill spawned processes when killed by a signal
Our new behavior for the `kill` command is to inject a thread that runs
the ExitProcess() function with an exit code of the form `128 + signal`.

This means that we can sort of emulate POSIX signaling behavior by
terminating the child processes with the very same signal.

Let's do that.

As a special quirk, we need to roll the _P_WAIT functionality of
spawnve() ourselves now, because we would otherwise not have a chance to
get the handle of the spawned process to kill.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

win32/process.c
@@ -30,6 +30,91 @@ static wchar_t **argv_to_wargv(char *const *argv)
 	return wargv;
 }
 
+static int exit_process(HANDLE process, int exit_code);
+
+static struct {
+	CRITICAL_SECTION mutex;
+	int nr, alloc;
+	HANDLE *h;
+} spawned_processes;
+
+#ifndef SIGRTMAX
+#define SIGRTMAX 63
+#endif
+
+static void kill_spawned_processes_on_signal(void)
+{
+        DWORD status;
+	int i, signal;
+
+	/*
+	 * Only continue if the process was terminated by a signal, as
+	 * indicated by the exit status (128 + sig_no).
+	 *
+	 * As we are running in an atexit() handler, the exit code has been
+	 * set at this stage by the ExitProcess() function already.
+	 */
+	if (!GetExitCodeProcess(GetCurrentProcess(), &status) ||
+			status <= 128 || status > 128 + SIGRTMAX)
+		return;
+	signal = status - 128;
+
+	EnterCriticalSection(&spawned_processes.mutex);
+	for (i = 0; i < spawned_processes.nr; i++) {
+		if (GetExitCodeProcess(spawned_processes.h[i], &status) &&
+				status == STILL_ACTIVE)
+			exit_process(spawned_processes.h[i], 128 + signal);
+		CloseHandle(spawned_processes.h[i]);
+	}
+	spawned_processes.nr = 0;
+	LeaveCriticalSection(&spawned_processes.mutex);
+}
+
+static void cull_exited_processes(void)
+{
+	DWORD status;
+	int i;
+
+	EnterCriticalSection(&spawned_processes.mutex);
+	/* cull exited processes */
+	for (i = 0; i < spawned_processes.nr; i++)
+		if (GetExitCodeProcess(spawned_processes.h[i], &status) &&
+				status != STILL_ACTIVE) {
+			CloseHandle(spawned_processes.h[i]);
+			spawned_processes.h[i] =
+				spawned_processes.h[--spawned_processes.nr];
+			i--;
+		}
+	LeaveCriticalSection(&spawned_processes.mutex);
+}
+
+static void exit_process_on_signal(HANDLE process)
+{
+	cull_exited_processes();
+	EnterCriticalSection(&spawned_processes.mutex);
+	/* grow array if necessary */
+	if (spawned_processes.nr == spawned_processes.alloc) {
+		int new_alloc = (spawned_processes.alloc + 8) * 3 / 2;
+		HANDLE *new_h = realloc(spawned_processes.h,
+				new_alloc * sizeof(HANDLE));
+		if (!new_h) {
+			LeaveCriticalSection(&spawned_processes.mutex);
+			return; /* punt */
+		}
+		spawned_processes.h = new_h;
+		spawned_processes.alloc = new_alloc;
+	}
+
+	spawned_processes.h[spawned_processes.nr++] = process;
+	LeaveCriticalSection(&spawned_processes.mutex);
+}
+
+void initialize_critical_sections(void)
+{
+	InitializeCriticalSection(&spawned_processes.mutex);
+	atexit(kill_spawned_processes_on_signal);
+}
+
 static intptr_t mingw_spawnve(int mode,
 		const char *cmd, char *const *argv, char *const *env)
 {
@@ -44,13 +129,36 @@ static intptr_t mingw_spawnve(int mode,
 		return -1;
 	}
 
-	ret = _wspawnve(mode, wcmd,
+	/*
+	 * We cannot use _P_WAIT here because we need to kill spawned processes
+	 * if we're killed, and _P_WAIT does not let us.
+	 */
+	ret = _wspawnve(_P_NOWAIT, wcmd,
 		(const wchar_t *const *)wargv, (const wchar_t *const *)wenv);
+
+	if (ret != (intptr_t)-1)
+		exit_process_on_signal((HANDLE)ret);
 
 	free(wargv);
 	free(wenv);
 
-	return ret;
+	if (ret == (intptr_t)-1 || mode != _P_WAIT)
+		return ret;
+
+	for (;;) {
+		DWORD exit_code;
+		WaitForSingleObject((HANDLE)ret, INFINITE);
+		if (!GetExitCodeProcess((HANDLE)ret, &exit_code)) {
+			errno = err_win_to_posix(GetLastError());
+			CloseHandle((HANDLE)ret);
+			return -1;
+		}
+		if (exit_code != STILL_ACTIVE) {
+			cull_exited_processes();
+			CloseHandle((HANDLE)ret);
+			return (intptr_t)exit_code;
+		}
+	}
 }
 
 int waitpid(pid_t pid, int *status, int options)
@@ -626,6 +734,7 @@ static int exit_process(HANDLE process, int exit_code)
 {
 	DWORD code;
 
+	cull_exited_processes();
 	if (GetExitCodeProcess(process, &code) && code == STILL_ACTIVE) {
 		static int initialized;
 		static LPTHREAD_START_ROUTINE exit_process_address;

spsforks/git-for-windows-busybox-w32

win32/process.c
@@ -37,6 +37,89 @@ static FAST_FUNC wchar_t **argv_to_wargv(char *const *argv)
 	return wargv;
 }
 
+static struct {
+	CRITICAL_SECTION mutex;
+	int nr, alloc;
+	HANDLE *h;
+} spawned_processes;
+
+static int kill_signal_by_handle(HANDLE process, int sig);
+
+static void kill_spawned_processes_on_signal(void)
+{
+	DWORD status;
+	int i;
+	int sig;
+
+	/*
+	 * Only continue if the process was terminated by a busybox-w32
+	 * signal, as indicated by the exit status (sig << 24).
+	 *
+	 * As we are running in an atexit() handler, the exit code has
+	 * been set at this stage by the ExitProcess() function already.
+	 */
+	if (!GetExitCodeProcess(GetCurrentProcess(), &status))
+		return;
+	sig = status >> 24;
+	if (sig == 0 || status != (DWORD)(sig << 24))
+		return;
+
+	EnterCriticalSection(&spawned_processes.mutex);
+	for (i = 0; i < spawned_processes.nr; i++) {
+		if (GetExitCodeProcess(spawned_processes.h[i], &status) &&
+				status == STILL_ACTIVE)
+			kill_signal_by_handle(spawned_processes.h[i], sig);
+		CloseHandle(spawned_processes.h[i]);
+	}
+	spawned_processes.nr = 0;
+	LeaveCriticalSection(&spawned_processes.mutex);
+}
+
+static void cull_exited_processes(void)
+{
+	DWORD status;
+	int i;
+
+	EnterCriticalSection(&spawned_processes.mutex);
+	/* cull exited processes */
+	for (i = 0; i < spawned_processes.nr; i++)
+		if (GetExitCodeProcess(spawned_processes.h[i], &status) &&
+				status != STILL_ACTIVE) {
+			CloseHandle(spawned_processes.h[i]);
+			spawned_processes.h[i] =
+				spawned_processes.h[--spawned_processes.nr];
+			i--;
+		}
+	LeaveCriticalSection(&spawned_processes.mutex);
+}
+
+static void exit_process_on_signal(HANDLE process)
+{
+	cull_exited_processes();
+	EnterCriticalSection(&spawned_processes.mutex);
+	/* grow array if necessary */
+	if (spawned_processes.nr == spawned_processes.alloc) {
+		int new_alloc = (spawned_processes.alloc + 8) * 3 / 2;
+		HANDLE *new_h = realloc(spawned_processes.h,
+				new_alloc * sizeof(HANDLE));
+		if (!new_h) {
+			LeaveCriticalSection(&spawned_processes.mutex);
+			return; /* punt */
+		}
+		spawned_processes.h = new_h;
+		spawned_processes.alloc = new_alloc;
+	}
+
+	spawned_processes.h[spawned_processes.nr++] = process;
+	LeaveCriticalSection(&spawned_processes.mutex);
+}
+
+void initialize_critical_sections(void)
+{
+	InitializeCriticalSection(&spawned_processes.mutex);
+	atexit(kill_spawned_processes_on_signal);
+}
+
 static intptr_t mingw_spawnve(int mode,
 		const char *cmd, char *const *argv, char *const *env)
 {
@@ -51,13 +134,36 @@ static intptr_t mingw_spawnve(int mode,
 		return -1;
 	}
 
-	ret = _wspawnve(mode, wcmd,
+	/*
+	 * We cannot use _P_WAIT here because we need to kill spawned processes
+	 * if we're killed, and _P_WAIT does not let us.
+	 */
+	ret = _wspawnve(_P_NOWAIT, wcmd,
 		(const wchar_t *const *)wargv, (const wchar_t *const *)wenv);
+
+	if (ret != (intptr_t)-1)
+		exit_process_on_signal((HANDLE)ret);
 
 	free(wargv);
 	free(wenv);
 
-	return ret;
+	if (ret == (intptr_t)-1 || mode != _P_WAIT)
+		return ret;
+
+	for (;;) {
+		DWORD exit_code;
+		WaitForSingleObject((HANDLE)ret, INFINITE);
+		if (!GetExitCodeProcess((HANDLE)ret, &exit_code)) {
+			errno = err_win_to_posix();
+			CloseHandle((HANDLE)ret);
+			return -1;
+		}
+		if (exit_code != STILL_ACTIVE) {
+			cull_exited_processes();
+			CloseHandle((HANDLE)ret);
+			return (intptr_t)exit_code;
+		}
+	}
 }
 
 pid_t FAST_FUNC waitpid(pid_t pid, int *status, int options)
@@ -972,6 +1078,7 @@ static int kill_signal_by_handle(HANDLE process, int sig)
 	DWORD thread_id;
 	HANDLE thread;
 
+	cull_exited_processes();
 	if (!INIT_PROC_ADDR(kernel32, ExitProcess) ||
 			!process_architecture_matches_current(process)) {
 		SetLastError(ERROR_ACCESS_DENIED);